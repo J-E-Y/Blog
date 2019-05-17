9986839302b0972e605ae46b5e6b713d0747b487
J-E-Y <rightfuljung@gmail.com>
2019-05-17 11:54:40 +0900

    category in post_list

--- blog/tests.py
+++ blog/tests.py
@@ -1,19 +1,68 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
-from .models import Post
+from .models import Post,Category
 from django.utils import timezone
 from django.contrib.auth.models import User
 
 
-def create_post(title, content, author):
+
+def create_category(name='life',description=''):
+    category, is_created = Category.objects.get_or_create(
+        name=name,
+        description=description
+    )
+
+    return category
+
+
+def create_post(title, content, author, category=None):
     blog_post = Post.objects.create(
         title=title,
         content=content,
         created=timezone.now(),
-        author=author
+        author=author,
+        category=category,
     )
 
     return blog_post
+
+
+
+
+class TestModel(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.author_000 = User.objects.create(username='smith', password='nopassword')
+
+
+
+    def test_category(self):
+        category = create_category()
+
+        post_000 = create_post(
+            title='The first post',
+            content='Hello World. We are the world.',
+            author=self.author_000,
+            category = category
+        )
+
+        self.assertEqual(category.post_set.count(), 1)
+
+
+
+
+    def test_post(self):
+
+        category = create_category()
+
+        post_000 = create_post(
+            title='The first post',
+            content='Hello World. We are the world.',
+            author=self.author_000,
+            category = category
+        )
+
+
 
 
 class TestView(TestCase):
@@ -26,14 +75,21 @@ class TestView(TestCase):
         self.assertIn('Blog', navbar.text)
         self.assertIn('About me', navbar.text)
 
-    def test_post_list(self):
+
+
+
+    def test_post_list_no_posts(self):
         response = self.client.get('/blog/')
+
         self.assertEqual(response.status_code, 200)
+
         soup = BeautifulSoup(response.content, 'html.parser')
         title = soup.title
 
         self.assertEqual(title.text, 'Blog')
+
         self.check_navbar(soup)
+
         # navbar = soup.find('div', id='navbar')
         # self.assertIn('Blog', navbar.text)
         # self.assertIn('About me', navbar.text)
@@ -41,19 +97,65 @@ class TestView(TestCase):
         self.assertEqual(Post.objects.count(), 0)
         self.assertIn('아직 게시물이 없습니다.', soup.body.text)
 
+
+
+
+
+    def test_post_list_with_post(self):
+
         post_000 = create_post(
             title='The first post',
             content='Hello World. We are the world.',
             author=self.author_000,
+
         )
 
+
+        post_000 = create_post(
+
+            title='The sceond post',
+            content='The sceond,The sceond,The sceond',
+            author=self.author_000,
+            category=create_category(name="political_society")
+
+        )
+
+
         self.assertGreater(Post.objects.count(), 0)
+
         response = self.client.get('/blog/')
+
         self.assertEqual(response.status_code, 200)
+
         soup = BeautifulSoup(response.content, 'html.parser')
         body = soup.body
+
+
         self.assertNotIn('아직 게시물이 없습니다.', body.text)
         self.assertIn(post_000.title, body.text)
+
+
+
+
+        #category card 에서
+
+        category_card = body.find("div", id="category-card")
+        # 1 우선 찾고 2 찾게 하기위해서 우선 html 가서 id 값을 지정한다 like <div class="card my-4" id="category-card">
+
+        self.assertIn("unclassified(1)",category_card.text)  # 미분류 있어야함(1)
+        self.assertIn("political_society(1)",category_card.text)   # 정치/사회있어야함(1)
+
+        ### mainn_div에는
+
+        main_div = body.find("div", id = "main_div") # 우선 찾고
+
+        self.assertIn("political_society", main_div.text) # main_div에는 정치/사회
+        self.assertIn("unclassified", main_div.text) # main_div에는 unclassified
+
+
+
+
+
 
     def test_post_detail(self):
         post_000 = create_post(
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post,Category
 from django.views.generic import ListView,DetailView
 
 # Create your views here.
@@ -10,9 +10,27 @@ class PostList(ListView):
     def get_queryset(self):
         return Post.objects.order_by('-created')
 
+        # 숫자 가져오는 함수 from django that it already made before
+
+    def get_context_data(self ,*, object_list=None, **kwargs):
+        context = super(PostList, self).get_context_data(**kwargs)
+        context['category_list'] = Category.objects.all()
+        context['posts_without_category'] = Post.objects.filter(category=None).count()
+        # get 하나만가져오는것
+        # all 다 가져오는것
+        # filter는 특정조건에 있는 것만 가져오는것
+
+        return context
+
+    # get 하나만가져오는것
+    # all 다 가져오는것
+    # filter는 특정조건에 있는 것만 가져오는것
+
 
 class PostDetail(DetailView):
     model = Post
+
+
 
 # def post_detail(request,pk):
 #     blog_post =\
