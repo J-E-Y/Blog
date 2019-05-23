a14912125320935ce25b2ef56dcf8ccae6415a8d
J-E-Y <rightfuljung@gmail.com>
2019-05-23 11:29:25 +0900

    post datai page with tag tested

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
-
-
 class Category(models.Model):
-    name =models.CharField(max_length=25,unique=True)
+    name = models.CharField(max_length=25, unique=True)
     description = models.TextField(blank=True)
 
     slug = models.SlugField(unique=True, allow_unicode=True)
@@ -17,15 +14,16 @@ class Category(models.Model):
     def get_absolute_url(self):
         return '/blog/category/{}/'.format(self.slug)
 
-
     class Meta:
-        verbose_name_plural = 'categories' # In admin you can change name on
+        verbose_name_plural = 'categories'
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    slug = models.SlugField(unique=True, allow_unicode=True)
 
-
-
-
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -33,11 +31,12 @@ class Post(models.Model):
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/%Y/%m/%d/', blank=True)
-    created = models.DateTimeField()
 
+    created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=True)
 
-    category = models.ForeignKey(Category,blank=True, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return '{} :: {}'.format(self.title, self.author)
