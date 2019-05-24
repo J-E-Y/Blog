fcca9a505aad4e6d1b207094a7cbeb56fa0337a9
J-E-Y <rightfuljung@gmail.com>
2019-05-24 09:45:56 +0900

    makrdownx on post; tags can be blank

--- my_site_prj/settings.py
+++ my_site_prj/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    'markdownx',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,8 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
 MEDIA_URL ='/media/'
+
+
+from datetime import datetime
+
+MARKDOWNX_MEDIA_PATH = datetime.now().strftime('markdownx/%Y/%m/%d')
